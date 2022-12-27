0a28fde39f1f39532b225678115c338b9c37f0f3
Matheus Mendes <matheusmendesreis@outlook.com>
2022-12-27 18:37:23 -0300

    info: lista de palavras criadas

---
+++ secrete-wordls/src/components/words/index.jsx
@@ -0,0 +1,9 @@
+
+export const WordList = {
+    carro: ["Motor", "Porta", "Capô"],
+    fruta: ["Banana", "Maça", "Pêra"],
+    corpo: ["Braço", "Perna", "Cerébro"],
+    computador: ["Mouse", "Teclado", "Monitor"],
+    programacao: ["Linguagem", "Framework", "JavaScript"],
+    alimento: ["Arroz", "Feijão", "Carne"]
+};
